d150ea688cc0b269dbd662dd4ff802f54c1a4306
Milindh Vijay <milindhvijay@gmail.com>
2025-03-24 23:28:39 +0530

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -314,13 +314,19 @@ EOF
         client_inet6=$(yq e ".remote_peer[$i].inet6_address" config.yaml)
         if [[ "$inet6_enabled" == "true" && $(ip -6 addr | grep -c 'inet6 [23]') -gt 0 ]]; then
             if [[ "$gateway_changed" == "true" || "$client_inet6" == "null" || -z "$client_inet6" ]]; then
-                client_inet6=$(find_next_inet6 "$base_inet6" "$server_inet6_mask" "${used_inet6s[@]}")
+                # Pass old client IPv6 to preserve offset when gateway changes
+                old_client_inet6=$(yq e ".remote_peer[$i].inet6_address" config.yaml)
+                client_inet6=$(find_next_inet6 "$base_inet6" "$server_inet6_mask" "$old_client_inet6" "${used_inet6s[@]}")
                 if [[ $? -ne 0 ]]; then
                     echo "$client_inet6"
                     return 1
                 fi
                 used_inet6s+=("$(echo "$client_inet6" | cut -d '/' -f 1)")
             fi
+        fi
+
+        yq e -i ".remote_peer[$i].inet_address = \"$client_inet\"" config.yaml.tmp
+        if [[ "$inet6_enabled" == "true" && $(ip -6 addr | grep -c 'inet6 [23]') -gt 0 ]]; then
             yq e -i ".remote_peer[$i].inet6_address = \"$client_inet6\"" config.yaml.tmp
         fi
 
@@ -451,17 +457,15 @@ generate_client_configs() {
         client_inet6=$(yq e ".remote_peer[$i].inet6_address" config.yaml)
         if [[ "$inet6_enabled" == "true" && $(ip -6 addr | grep -c 'inet6 [23]') -gt 0 ]]; then
             if [[ "$gateway_changed" == "true" || "$client_inet6" == "null" || -z "$client_inet6" ]]; then
-                client_inet6=$(find_next_inet6 "$base_inet6" "$server_inet6_mask" "${used_inet6s[@]}")
+                # Pass old client IPv6 to preserve offset when gateway changes
+                old_client_inet6=$(yq e ".remote_peer[$i].inet6_address" config.yaml)
+                client_inet6=$(find_next_inet6 "$base_inet6" "$server_inet6_mask" "$old_client_inet6" "${used_inet6s[@]}")
                 if [[ $? -ne 0 ]]; then
                     echo "$client_inet6"
                     return 1
                 fi
                 used_inet6s+=("$(echo "$client_inet6" | cut -d '/' -f 1)")
             fi
-        fi
-
-        yq e -i ".remote_peer[$i].inet_address = \"$client_inet\"" config.yaml.tmp
-        if [[ "$inet6_enabled" == "true" && $(ip -6 addr | grep -c 'inet6 [23]') -gt 0 ]]; then
             yq e -i ".remote_peer[$i].inet6_address = \"$client_inet6\"" config.yaml.tmp
         fi
 
