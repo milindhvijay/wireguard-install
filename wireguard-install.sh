29caf254d07c9d6450cafa176256bde27ff3378a
Milindh Vijay <milindhvijay@gmail.com>
2025-03-25 01:58:30 +0530

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -295,7 +295,7 @@ EOF
 
         # Handle IP assignment
         client_inet=$(yq e ".remote_peer[$i].inet_address" config.yaml)
-        if [[ "$inet_enabled" == "true" && $(ip -4 addr | grep -c 'inet ') -gt 0 && ( "$client_inet" == "null" || -z "$client_inet" ) ]]; then
+        if [[ "$inet_enabled" == "true" && ( "$client_inet" == "null" || -z "$client_inet" ) ]]; then
             client_inet=$(find_next_inet "$base_inet" "$server_inet_mask" "${used_inets[@]}")
             if [[ $? -ne 0 ]]; then
                 echo "$client_inet"
@@ -501,15 +501,19 @@ generate_client_configs() {
 
         # Recalculate IPs if gateway changed or missing
         client_inet=$(yq e ".remote_peer[$i].inet_address" config.yaml)
-        if [[ "$inet_enabled" == "true" && ( "$client_inet" == "null" || -z "$client_inet" || "$gateway_changed" == "true" ) ]]; then
+        if [[ "$inet_enabled" == "true" && $(ip -4 addr | grep -c 'inet ') -gt 0 && ( "$client_inet" == "null" || -z "$client_inet" ) ]]; then
             client_inet=$(find_next_inet "$base_inet" "$server_inet_mask" "${used_inets[@]}")
             if [[ $? -ne 0 ]]; then
                 echo "$client_inet"
                 return 1
             fi
             used_inets+=("$(echo "$client_inet" | cut -d '/' -f 1)")
+            yq e -i ".remote_peer[$i].inet_address = \"$client_inet\"" config.yaml.tmp
             echo "DEBUG: Assigned new inet address for $client_name: $client_inet"
+        else
+            used_inets+=("$(echo "$client_inet" | cut -d '/' -f 1)")
         fi
+
 
         client_inet6=$(yq e ".remote_peer[$i].inet6_address" config.yaml)
         if [[ "$inet6_enabled" == "true" && $(ip -6 addr | grep -c 'inet6 [23]') -gt 0 && ( "$client_inet6" == "null" || -z "$client_inet6" || "$gateway_changed" == "true" ) ]]; then
